01d8336b98ffa91e1629a2ed3dfe0565b4c1aa42
PauloRoberto <probertolp70@gmail.com>
2022-08-04 16:00:45 -0300

    difinindo o fim do game

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ function update (event){
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
+
 //Criação dos movimentos da cobrinha//
 function iniciarJogo(){
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
@@ -48,6 +49,12 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game Over :'(")
+        }
+    }
 
     criarBG();
     criarCobrinha();
